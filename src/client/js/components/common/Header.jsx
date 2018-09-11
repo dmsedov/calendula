fcd4984fd4e495d311f9c004e9c243df386e5017
Dmitry Sedov <dmsedov92@gmail.com>
2018-09-11 23:00:13 +0300

    feature close nav menu in case click beyond of menu

--- src/client/js/components/common/Header.jsx
+++ src/client/js/components/common/Header.jsx
@@ -3,6 +3,16 @@ import { Link } from 'react-router-dom';
 import Menu from '../content/Menu';
 
 export default class Header extends React.Component {
+  handleClickBeyoundOfMenu = () => {
+    const { closeNavMenu } = this.props;
+    closeNavMenu();
+  }
+
+  handleClickOnLogo = () => {
+    const { closeNavMenu } = this.props;
+    closeNavMenu();
+  } // требует рефакторинга обработчиков много повторений!!!
+
   renderSearhEl = () => {
     const { handleOpenModal } = this.props;
     return <button type="button" className="search btn btn-primary" data-toggle="modal" onClick={handleOpenModal('Search')} />;
@@ -15,15 +25,17 @@ export default class Header extends React.Component {
       location: { pathname },
       modalName,
       renderModalItemByName,
+      isExpandNavMenu,
     } = this.props;
 
     return (
       <header>
         <div className="menu-position bg-primary">
           <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-            <Link className="navbar-brand" to="/">Calendula</Link>
+            <Link className="navbar-brand" to="/" onClick={this.handleClickOnLogo}>Calendula</Link>
             {userStatus === 'authenticated' && pathname === '/calendar' ? this.renderSearhEl() : null}
             <Menu {...this.props} />
+            {isExpandNavMenu ? <div className="menu-layout" onClick={this.handleClickBeyoundOfMenu}></div> : null}
           </nav>
         </div>
         {renderModalItemByName(modalName)}
