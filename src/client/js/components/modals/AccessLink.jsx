ece7c7df6c9db82227505052b2e7647964be352c
Dmitry Sedov <dmsedov92@gmail.com>
2018-10-08 23:35:49 +0300

    feature copy link into buffer AccessLink.js

--- src/client/js/components/modals/AccessLink.jsx
+++ src/client/js/components/modals/AccessLink.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import { ModalHeader, ModalBody, Button } from 'reactstrap';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 export default class AccessLink extends React.Component {
+  state = {
+    value: '',
+    copied: false,
+  };
+
   componentDidMount() {
+    const mockLink = 'http://calendula.me:8888';
+    this.setState({ value: mockLink });
+  }
+
+  handleCloseModal = () => {
+    const { toggle } = this.props;
+    toggle();
   }
 
   render() {
+    const { value } = this.state;
+
     return (
       <div id="access-link">
         <ModalHeader>
@@ -14,8 +29,13 @@ export default class AccessLink extends React.Component {
         <ModalBody>
           <div className="content">
             <p className="description">Нажмите Скопировать для копирования ссылки в буфер</p>
-            <input className="access-link" type="text" name="accessLink" readOnly />
-            <Button color="primary" size="sm">Скопировать</Button>
+            <input className="access-link" type="text" name="accessLink" value={value} readOnly />
+            <CopyToClipboard
+              text={value}
+              onCopy={() => { this.setState({ copied: !this.state.copied }); }}
+            >
+              <Button onClick={this.handleCloseModal} color="primary" size="sm">Скопировать</Button>
+            </CopyToClipboard>
           </div>
         </ModalBody>
       </div>
