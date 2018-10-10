02c8c5c15ab15c53c7666430e53e5639fa0f8232
Dmitry Sedov <dmsedov92@gmail.com>
2018-10-10 10:35:28 +0300

    add error reducer according to reducers/error.test

--- src/client/js/reducers/error.js
+++ src/client/js/reducers/error.js
@@ -1,10 +1,13 @@
-import resetErrorMsg from '../actions/error';
+import resetErrAction from '../actions/error';
 import initGlobalState from './initGlobalState';
 
 const { error } = initGlobalState;
+const resetErrorMsg = resetErrAction();
 
 export default (state = error, { type, payload }) => {
   if (type === resetErrorMsg.type) {
+    return null;
+  } else if (payload) {
     return payload;
   }
   return state;
