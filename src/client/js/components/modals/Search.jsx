0cc8853e97b1dcf70ac05f44669ca77c5850b43b
Dmitry Sedov <dmsedov92@gmail.com>
2018-10-26 16:38:33 +0300

    refactor usage of variables in scss components

--- src/client/js/components/modals/Search.jsx
+++ src/client/js/components/modals/Search.jsx
@@ -15,7 +15,7 @@ export default class Search extends React.Component {
       <ModalBody key={_.uniqueId()}>
         <form className="form-inline my-2 my-lg-0" onSubmit={this.handleSearchSubmit}>
           <input className="form-control mr-sm-2" type="search" placeholder="Искать" aria-label="Search" />
-          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Искать</button>
+          <button className="btn" type="submit">Искать</button>
         </form>
       </ModalBody>,
     ];
