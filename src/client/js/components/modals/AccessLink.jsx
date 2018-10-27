6a2da62446be3ec42fe0c7b67efb4ad91103d4aa
Dmitry Sedov <dmsedov92@gmail.com>
2018-10-27 22:28:27 +0300

    refactor access-form js and css files

--- src/client/js/components/modals/AccessLink.jsx
+++ src/client/js/components/modals/AccessLink.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ModalHeader, ModalBody, Button } from 'reactstrap';
+import { ModalHeader, ModalBody, ModalFooter, Button } from 'reactstrap';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { Icon } from 'antd';
 
@@ -17,8 +17,6 @@ export default class AccessLink extends React.Component {
   render() {
     const { accessLink } = this.props;
     const iconStyle = {
-      fontSize: '1.5rem',
-      cursor: 'pointer',
       pointerEvents: accessLink ? 'visiblePainted' : 'none',
     };
 
@@ -30,21 +28,29 @@ export default class AccessLink extends React.Component {
         <ModalBody>
           <div className="access-form__content">
             <p className="access-form__info-text">Сгенерируйте ссылку для доступа в календарь.</p>
-            <input className="access-form__access-link" type="text" value={accessLink} readOnly />
-            <CopyToClipboard
-              text={accessLink}
-            >
-              <Icon
-                type="copy"
-                theme="twoTone"
-                style={iconStyle}
-                twoToneColor="#00f"
-                onClick={this.handleCloseModal}
+            <div className="access-form__field">
+              <input
+                className="form-control access-form__access-link"
+                type="text"
+                value={accessLink}
+                readOnly
               />
-            </CopyToClipboard>
-            <Button className="access-form__gen-link-btn" onClick={this.handleGenAccessLink}>Сгенерировать ссылку</Button>
+              <CopyToClipboard
+                text={accessLink}
+              >
+                <Icon
+                  type="copy"
+                  theme="filled"
+                  onClick={this.handleCloseModal}
+                  style={iconStyle}
+                />
+              </CopyToClipboard>
+            </div>
           </div>
         </ModalBody>
+        <ModalFooter>
+          <Button className="access-form__gen-link-btn" onClick={this.handleGenAccessLink}>Сгенерировать ссылку</Button>
+        </ModalFooter>
       </div>
     );
   }
