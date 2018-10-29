0549e82972740502bc7121bf046a5d0f0ce46015
Dmitry Sedov <dmsedov92@gmail.com>
2018-10-30 01:12:33 +0300

    feature render day events in calendar cells)

--- src/client/js/actions/calendar.js
+++ src/client/js/actions/calendar.js
@@ -4,7 +4,7 @@ import errorHandler from '../helpers/errorHandler';
 import errors from '../errors';
 
 
-// import calendarJSON from './mockCalendar.json'; // delete in future
+import calendarJSON from './calendar.json'; // delete in future
 
 export const fetchCalendarRequest = createAction('FETCH_CALENDAR_REQUEST');
 
@@ -15,14 +15,14 @@ export const fetchCalendarFailure = createAction('FETCH_CALENDAR_FAILURE');
 export const fetchCalendar = monthId => async (dispatch) => {
   const { getCalendar } = apiCall.private;
   dispatch(fetchCalendarRequest());
-  try {
-    const {
-      data: { calendar },
-    } = await getCalendar(monthId);
-    // const calendar = calendarJSON;
+  // try {
+  //   const {
+  //     data: { calendar },
+  //   } = await getCalendar(monthId);
+    const calendar = calendarJSON;
     dispatch(fetchCalendarSuccess({ calendar }));
-  } catch (e) {
-    const errCode = errorHandler(e);
-    dispatch(fetchCalendarFailure({ error: errors[errCode] }));
-  }
+  // } catch (e) {
+  //   const errCode = errorHandler(e);
+  //   dispatch(fetchCalendarFailure({ error: errors[errCode] }));
+  // }
 };
--- src/client/js/components/content/Calendar.jsx
+++ src/client/js/components/content/Calendar.jsx
@@ -4,9 +4,15 @@ import cn from 'classnames';
 import _ from 'lodash';
 
 export default class Calendar extends React.Component {
+  state = { isOpenEventsList: false }
+
   componentDidMount() {
     const { fetchCalendar } = this.props;
     fetchCalendar();
+  }
+
+  toggle = () => {
+    this.setState({ isOpenEventsList: !this.state.isOpenEventsList });
   }
 
   makeShortWeekDayNames = (weekDay) => {
@@ -20,6 +26,17 @@ export default class Calendar extends React.Component {
       Воскресенье: 'Вс',
     }[weekDay];
   }
+
+  handleClickOnEvent = id => () => {
+    console.log(id, 'getEventData on this id');
+  }
+
+  makeDayEventsList = eventsList => eventsList.map((eventData) => {
+    const { id, title } = eventData;
+    return (
+      <li key={id} className="calendar__day-event" onClick={this.handleClickOnEvent(id)}>{title}</li>
+    );
+  });
 
   renderPanelWithWeekDayNames = () => {
     const dayNames = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
@@ -63,7 +80,9 @@ export default class Calendar extends React.Component {
         <div className="calendar__days">
           {!isLessThanLgScreen ? this.renderPanelWithWeekDayNames() : null}
           {days.map((_day) => {
-            const { id, number, week_day, month_id } = _day;
+            const { id, number, week_day, month_id, events_data: { events } } = _day;
+
+            const eventsListItems = this.makeDayEventsList(events);
 
             const classNamesDay = cn({
               'calendar__day': true,
@@ -75,13 +94,14 @@ export default class Calendar extends React.Component {
             });
 
             return (
-              <div key={id} className={classNamesDay}>
+              <div key={id} className={classNamesDay} onClick={this.toggle}>
                 <div className="calendar__day-of-week">
                   {isLessThanLgScreen && <span className="calendar__day-name">{this.makeShortWeekDayNames(week_day)}</span>}
                   <span className={classNamesDayNumber}>{number}</span>
                 </div>
                 <div className="calendar__day-content">
                   <ul className="calendar__day-events">
+                    {eventsListItems}
                   </ul>
                 </div>
               </div>
