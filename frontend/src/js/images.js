fc945e4328c097d15f4d8869c652c901342677ef
Dmitry Sedov <dmsedov92@gmail.com>
2018-10-31 22:33:07 +0300

    create images common file with imports

---
+++ frontend/src/js/images.js
@@ -0,0 +1,11 @@
+import preloaderGif from '../images/icons/preloader.gif';
+import googleIcon from '../images/icons/google-32px.png';
+import facebookIcon from '../images/icons/facebook-32px.png';
+import searchIcon from '../images/icons/search.png';
+
+export default {
+  preloaderGif,
+  googleIcon,
+  facebookIcon,
+  searchIcon,
+};
